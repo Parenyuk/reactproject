ad319610d5de6e23aa653b9a9031c161c7e8c7a3
Victor <ParenVic@i.ua>
2020-07-01 10:57:38 +0300

    'Deploy to GitHub Pages 2'

--- src/components/Counter/CounterButton/CounterButton.jsx
+++ src/components/Counter/CounterButton/CounterButton.jsx
@@ -5,7 +5,11 @@ import React from "react";
     return (
         <div>
             <button onClick={props.onButtonClick} onChange={props.onInputMessage}>button</button>
+            <div>
+                we
+            </div>
         </div>
+
     )
 }
 export default CounterButton;
